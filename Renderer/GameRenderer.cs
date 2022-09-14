ad12d766a330164bc28f9ecdb746499d153ad48c
Balázs Tóth <grandmastar27@gmail.com>
2022-09-14 22:00:30 +0200

    Added base game and ui renderer

---
+++ Renderer/GameRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Model.Game;
+using Model.UI;
+using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer
+{
+    public class GameRenderer
+    {
+        private IGameModel gameModel;
+        private string assetsPath;
+
+        public GameRenderer(IGameModel gameModel, string assetsPath)
+        {
+            this.gameModel = gameModel;
+            this.assetsPath = assetsPath;
+        }
+
+        public void Draw(RenderTarget window)
+        {
+            //window.Draw(DrawablePlayer());
+            DrawableTilemap(window);
+        }
+
+        //private Drawable DrawablePlayer()
+        //{
+        //    gameModel.Player.Texture = new Texture(Path.Combine(assetsPath, "player.png"));
+        //    return gameModel.Player;
+        //}
+
+        private void DrawableTilemap(RenderTarget window)
+        {
+            for (int i = 0; i < gameModel.Map.Vertices.Count; i++)
+            {
+                window.Draw(gameModel.Map.Vertices[i], PrimitiveType.Quads, new(BlendMode.Alpha, Transform.Identity, gameModel.Map.TilesetTexture, null));
+            }
+        }
+    }
+}
