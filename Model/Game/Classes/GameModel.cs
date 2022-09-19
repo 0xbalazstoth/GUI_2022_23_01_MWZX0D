922dd157677e0a4848eb173970458814f133c522
Balazs Toth <grandmastar27@gmail.com>
2022-09-19 21:07:03 +0200

    Fixed merge model

--- Model/Game/Classes/GameModel.cs
+++ Model/Game/Classes/GameModel.cs
@@ -1,8 +1,5 @@
 ﻿using SFML.Graphics;
-<<<<<<< HEAD
 using SFML.System;
-=======
->>>>>>> 4a6ff04bd44cea7113df8ad930cabf25a16cdb90
 using System.Collections.Generic;
 
 namespace Model.Game.Classes
@@ -17,11 +14,8 @@ namespace Model.Game.Classes
         public EnemyModel Enemy { get; set; }
         public List<ChestModel> Chests { get ; set ; }
         public Dictionary<MovementDirection, Movement> MovementDirections { get; set; }
-<<<<<<< HEAD
         public Vector2f MousePositionWindow { get; set; }
         public Vector2f WorldPositionInCamera { get; set; }
         public List<BulletModel> Bullets { get; set; }
-=======
->>>>>>> 4a6ff04bd44cea7113df8ad930cabf25a16cdb90
     }
 }
