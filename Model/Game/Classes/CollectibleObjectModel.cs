8ac21f2640bc03126dde5fe908505ef40ac083d1
Balázs Tóth <grandmastar27@gmail.com>
2022-10-03 22:02:12 +0200

    Coin animation added to collectible items

---
+++ Model/Game/Classes/CollectibleObjectModel.cs
@@ -0,0 +1,16 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Game.Classes
+{
+    public class CollectibleObjectModel
+    {
+        public Guid Id { get; set; }
+        public CircleShape Item { get; set; }
+        public bool IsItemCollected { get; set; }
+    }
+}
